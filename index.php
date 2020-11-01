33af50be478cce036644e34137d2f3a526959cce
chanlazaro <lazarochan03@gmail.com>
2020-11-01 15:36:29 +0800

    added index.php with answer

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-<?= "Hello world" ?>
+<?php
+  class User
+  {
+    function __construct($name, $username, $email)
+    {
+      $this->name = $name;
+      $this->username = $username;
+      $this->email = $email;
+    }
+
+    function addFriend()
+    {
+      echo $this->name . " added a friend";
+      echo "<br>";
+    }
+
+    function postStatus()
+    {
+      echo $this->name . " postedd a status";
+      echo "<br>";
+    }
+  }
+
+  $user1 = new User("Christian Lazaro", "chansuu", "lazarochan03@gmail.com");
+  $user1->addFriend();
+  $user1->postStatus();
+?>
